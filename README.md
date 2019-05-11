a7925d789a3ea245703dfa8f34c1ce41252c1b1a
Florian CALLEWAERT <florian.callewaert@protonmail.com>
2019-05-11 09:07:42 +0200

    symfony coding standard (1)

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # course-php-legacy-code
+
+## TODO LIST
+
+- respect Symfony Coding Standard
+
+- implement SOLID principles
+
+    - Single responsability
+        Only one reason to modify a specific class
+
+    - Open-Closed
+        Open for extension, closed for modification
+
+    - Liskov substitution
+        Replace an instance by an instance of a subclass should not change code correctness
+
+    - Interface segregation
+        Use multiple specific interface instead of global heavy interface
+
+    - Dependancy inversion
+        Depend on interface, not implementation
+
+- use calisthenics object
+
+1. One indentation level
+2. No else
--- www/core/Validator.class.php
+++ www/core/Validator.class.php
@@ -1,79 +1,109 @@
 <?php
-class Validator{
 
-	public $errors = [];
+class Validator
+{
+    public $errors = [];
 
-	public function __construct( $config, $data ){
+    public function __construct (
+        $config,
+        $data
+    ) {
 
-		if(count($data) != count($config["data"])){
-			die("Tentative : faille XSS");
-		}
-
-
-			
-		foreach ($config["data"] as $name => $info) {
-
-			if( !isset($data[$name] )){
-				die("Tentative : faille XSS");
-			}else{
-
-				if( ($info["required"]??false) && !self::notEmpty( $data[$name] ) ){
-					$this->errors[]=$info["error"];
-				}
+        if (
+            count($data) != count($config["data"])
+        ) {
+            die("Tentative : faille XSS");
+        }
 
 
-				if(isset($info["minlength"]) && !self::minLength($data[$name], $info["minlength"])){
-					$this->errors[]=$info["error"];
-				}
+        foreach (
+            $config["data"] as $name => $info
+        ) {
+            if (
+                !isset($data[$name])
+            ) {
+                die("Tentative : faille XSS");
+            } else {
 
-				if(isset($info["maxlength"]) && !self::maxLength($data[$name], $info["maxlength"])){
-					$this->errors[]=$info["error"];
-				}
+                if (
+                    ($info["required"] ?? false)
+                    AND !self::notEmpty($data[$name])
+                ) {
+                    $this->errors[]= $info["error"];
+                }
 
-				if($info["type"]=="email" && !self::checkEmail($data[$name])){
-					$this->errors[]=$info["error"];
-				}
+                if (
+                    isset($info["minlength"])
+                    AND !self::minLength($data[$name], $info["minlength"])
+                ) {
+                    $this->errors[]= $info["error"];
+                }
 
-				if(isset($info["confirm"]) && $data[$name] != $data[$info["confirm"]]){
-					$this->errors[]=$info["error"];
-				}
+                if (
+                    isset($info["maxlength"])
+                    AND !self::maxLength($data[$name], $info["maxlength"])
+                ) {
+                    $this->errors[]= $info["error"];
+                }
 
-				else if($info["type"]=="password" && !self::checkPassword($data[$name])){
-					$this->errors[]=$info["error"];
-				}
+                if (
+                    $info["type"] === "email"
+                    AND !self::checkEmail($data[$name])
+                ) {
+                    $this->errors[]= $info["error"];
+                }
 
-			}
+                if (
+                    isset($info["confirm"])
+                    AND $data[$name] !== $data[$info["confirm"]]
+                ) {
+                    $this->errors[]= $info["error"];
+                } else if (
+                    $info["type"] === "password"
+                    AND !self::checkPassword($data[$name])
+                ) {
+                    $this->errors[]= $info["error"];
+                }
+            }
 
-		}
+        }
+    }
 
-	}
+    public static function notEmpty(
+        $string
+    ) {
+        return !empty(trim($string));
+    } 
 
+    public static function minLength(
+        $string,
+        $length
+    ) {
+        return strlen(trim($string)) >= $length;
+    } 
 
-	public static function notEmpty($string){
-		return !empty(trim($string));
-	} 
+    public static function maxLength(
+        $string,
+        $length
+    ) {
+        return strlen(trim($string)) <= $length;
+    }
 
-	public static function minLength($string, $length){
-		return strlen(trim($string))>=$length;
-	} 
+    public static function checkEmail(
+        $string
+    ) {
+        return filter_var(trim($string), FILTER_VALIDATE_EMAIL);
+    }
 
-	public static function maxLength($string, $length){
-		return strlen(trim($string))<=$length;
-	}
-
-	public static function checkEmail($string){
-		return filter_var(trim($string), FILTER_VALIDATE_EMAIL);
-	}
-
-	public static function checkPassword($string){
-		return (
-					preg_match("#[a-z]#", $string) && 
-					preg_match("#[A-Z]#", $string) && 
-					preg_match("#[0-9]#", $string));
-	}
-
-
-
+    public static function checkPassword(
+        $string
+    ) {
+        return (
+            preg_match("#[a-z]#", $string)
+            AND preg_match("#[A-Z]#", $string)
+            AND preg_match("#[0-9]#", $string)
+        );
+    }
 }
 
 
